b0cb7ec36e8a354fc74315816554cb029ccb5283
christopher-landress <christopher.landress@gmail.com>
2018-09-24 20:17:44 -0400

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,3 @@
-// Copyright (C) 2012 Sean Middleditch
-// All rights reserved.
-// This code is for illustration purposes for a talk.
-// Do not use in your own projects for any reason.  Even
-// if the copyright made it legal, you don't _want_ to
-// use this code in a real project.  Seriously.
 #include "../include/observed.h"
 #include "../include/observer.h"
 
